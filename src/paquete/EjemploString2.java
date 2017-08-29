67aaa176a93f17b99352c6c1db981153eb36fa7c
Martin21594 <martin210593@hotmail.com>
2017-08-29 14:50:48 -0300

    ejemplo de String CharaT

--- src/paquete/EjemploString2.java
+++ src/paquete/EjemploString2.java
@@ -21,6 +21,19 @@ public class EjemploString2 {
         System.out.println(letra1);//informa la posicion del String
         System.out.println(letra2);
         System.out.println(letra3);
+        
+        String cadena2="CasaRodante";
+        
+        char letra01=cadena2.charAt(0); //indica la posicion que va a mostar 
+        char letra02=cadena2.charAt(1);
+        char letra03=cadena2.charAt(2);
+        System.out.println(letra01);//informa la posicion del String
+        System.out.println(letra02);
+        System.out.println(letra03);
+        
+        
+        
+        
     }
 }
     
