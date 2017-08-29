a07f1a29b27d8b26676b90a2f0848b1a18a3e65e
Martin21594 <martin210593@hotmail.com>
2017-08-29 14:26:14 -0300

    metodos

---
+++ src/paquete/EjemploString2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author Español
+ */
+public class EjemploString2 {
+    
+    
+       public static void main(String[] args) {
+        String cadena="Automovil";
+        
+        char letra1=cadena.charAt(0);
+        char letra2=cadena.charAt(2);
+        char letra3=cadena.charAt(5);
+        System.out.println(letra1);
+        System.out.println(letra2);
+        System.out.println(letra3);
+    }
+}
+    
+
